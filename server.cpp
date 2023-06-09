1b3579749e7090e36c98ce9f37f8193fefaf0003
Abdelhak Elyakoubi <aelyakou@e1r9p13.1337.ma>
2023-06-09 04:05:17 +0100

    started listening and connecting clients (primitively)

--- server.cpp
+++ server.cpp
@@ -1,2 +1,33 @@
-#include "server.hpp"
+#include "Server.hpp"
 
+Server::Server(int port)
+{
+    this->srv_port = port;
+}
+
+Server::~Server()
+{
+}
+
+void Server::run()
+{
+    std::cout << "Server is running on port " << this->srv_port << std::endl;
+
+    //TODO: here is where we are going to create the socket and start listening for connections;
+    this->srv_socket = socket(AF_INET, SOCK_STREAM, 0);
+    fcntl(this->srv_socket, F_SETFL, O_NONBLOCK);
+
+    sockaddr_in sckt;
+    sockaddr_in client;
+    socklen_t   client_len;
+    sckt.sin_family = AF_INET;
+    sckt.sin_port = htons(this->srv_port);
+    sckt.sin_addr.s_addr = inet_addr("0.0.0.0");
+
+
+    bind(this->srv_socket, (sockaddr *)&sckt, sizeof(sockaddr_in));
+
+    listen(this->srv_socket, 10);
+    accept(this->srv_socket, (sockaddr *)&client, &client_len);
+    
+}
--- server.hpp
+++ server.hpp
@@ -1,10 +1,28 @@
-#ifndef SERVER_HPP
-#define SERVER_HPP
+#pragma once
 
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "Client.hpp"
 
 
 //TODO create whatever classes we need to make the server work;
 
-#endif
+class Client;
+
+class Server
+{
+    private:
+        int srv_socket;
+        int srv_port;
+        std::vector<Client> clients;
+    public:
+        Server(int port);
+        ~Server();
+        void run();
+};
